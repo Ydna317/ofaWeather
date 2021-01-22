16a6d318b22e81d060f9e88330a0e505819b2f3b
Andy Nguyen <Ydna317@gmail.com>
2021-01-22 14:03:59 -0800

    finalizing details and adding screenshots--what a process....

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# ofaWeather
+Created index.html
+    Added bootstrap forms for layout
+    Added required buttons for search and reset
+    Added forms for jumbotron
+    Added class containers and rows size 3 and 9 to match example
+    Added linking script srcs for function
+Created script.js
+    Created js outline for needs of the page
+    Added functions to retrieve necessary information for weatherapp
+    Applied personal API key
+        Could not retrieve five day forecast
+            Found out it was because I was missing a character in the API key... -____-
+    Created reset history button
+        Did not work
+            Abandoned idea
+    Tested application
+    Everything worked in order.
+        Found several errors in inspection tool
+            Cleared as I could
+Created style.css
+    Abandoned style.css
+
+![ofaWeatherSS1](https://user-images.githubusercontent.com/74637520/105553517-6732ff80-5cba-11eb-80f5-0cbc003e748e.png)
+![ofaWeatherSS2](https://user-images.githubusercontent.com/74637520/105553522-67cb9600-5cba-11eb-91e6-e145a2d07444.png)
+![ofaWeatherSS3](https://user-images.githubusercontent.com/74637520/105553525-67cb9600-5cba-11eb-8d92-c2b83b04d612.png)
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let apiKey="3c8c1d9e76eadd902a53aa3ec8156094"
-$(document).ready(function() {
-  $("#search-button").on("click", function() {
+let apiKey = "3c8c1d9e76eadd902a53aa3ec8156094"
+$(document).ready(function () {
+  $("#search-button").on("click", function () {
     let findCity = $("#search-value").val();
     $("#search-value").val("");
     searchWeather(findCity);
   });
-  $(".history").on("click", "li", function() {
+  $(".history").on("click", "li", function () {
     searchWeather($(this).text());
   });
   function makeRow(text) {
@@ -17,16 +17,13 @@ $(document).ready(function() {
       type: "GET",
       url: "http://api.openweathermap.org/data/2.5/weather?q=" + findCity + "&appid=3c8c1d9e76eadd902a53aa3ec8156094&units=imperial",
       dataType: "json",
-      success: function(data) {
-        // create history link for this search
+      success: function (data) {
         if (history.indexOf(findCity) === -1) {
           history.push(findCity);
           window.localStorage.setItem("history", JSON.stringify(history));
           makeRow(findCity);
         }
-        // clear any old content
         $("#today").empty();
-        // create html content for current weather
         let title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
         let card = $("<div>").addClass("card");
         let wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
@@ -34,12 +31,10 @@ $(document).ready(function() {
         let temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °F");
         let cardBody = $("<div>").addClass("card-body");
         let img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-        // merge and add to page
         title.append(img);
         cardBody.append(title, temp, humid, wind);
         card.append(cardBody);
         $("#today").append(card);
-        // call follow-up api endpoints
         getForecast(findCity);
         getUVIndex(data.coord.lat, data.coord.lon);
       }
@@ -50,14 +45,10 @@ $(document).ready(function() {
       type: "GET",
       url: "http://api.openweathermap.org/data/2.5/forecast?q=" + findCity + "&appid=3c8c1d9e76eadd902a53aa3ec8156094&units=imperial",
       dataType: "json",
-      success: function(data) {
-        // overwrite any existing content with title and empty row
+      success: function (data) {
         $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
-        // loop over all forecasts (by 3-hour increments)
         for (let i = 0; i < data.list.length; i++) {
-          // only look at forecasts around 3:00pm
           if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
-            // create html elements for a bootstrap card
             let col = $("<div>").addClass("col-md-2");
             let card = $("<div>").addClass("card bg-primary text-white");
             let body = $("<div>").addClass("card-body p-2");
@@ -65,7 +56,6 @@ $(document).ready(function() {
             let img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
             let p1 = $("<p>").addClass("card-text").text("Temp: " + data.list[i].main.temp_max + " °F");
             let p2 = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
-            // merge together and put on page
             col.append(card.append(body.append(title, img, p1, p2)));
             $("#forecast .row").append(col);
           }
@@ -78,10 +68,9 @@ $(document).ready(function() {
       type: "GET",
       url: "http://api.openweathermap.org/data/2.5/uvi?appid=3c8c1d9e76eadd902a53aa3ec8156094&lat=" + lat + "&lon=" + lon,
       dataType: "json",
-      success: function(data) {
+      success: function (data) {
         let uv = $("<p>").text("UV Index: ");
         let btn = $("<span>").addClass("btn btn-sm").text(data.value);
-        // change color depending on uv value
         if (data.value < 3) {
           btn.addClass("btn-success");
         }
@@ -95,10 +84,9 @@ $(document).ready(function() {
       }
     });
   }
-  // get current history, if any
   let history = JSON.parse(window.localStorage.getItem("history")) || [];
   if (history.length > 0) {
-    searchWeather(history[history.length-1]);
+    searchWeather(history[history.length - 1]);
   }
   for (let i = 0; i < history.length; i++) {
     makeRow(history[i]);
